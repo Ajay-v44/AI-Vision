5398389e8df8fe9436a37a24adf96dfe4b84de53
Ajay v <vajay4834@gmail.com>
2023-07-01 16:08:09 +0530

    Create post page completed

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,20 +1,41 @@
-import React ,{useState} from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { preview } from '../assets'
-import {getRandomPrompt} from '../utils';
-import { FormField,Loader } from '../components';
+import { getRandomPrompt } from '../assets/utils';
+import { FormField, Loader } from '../components';
 const CreatePost = () => {
-  const navigate =useNavigate();
+  const navigate = useNavigate();
   const [form, setForm] = useState({
-    name:'',
-    prompt:'',
-    photo:'',
+    name: '',
+    prompt: '',
+    photo: '',
   });
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false)
+  const handleSubmit = () => {
+
+  }
+  const handleChange = (e) => {
+
+  }
+  const handleSurpriseMe = () => {
+
+  }
   return (
-    <section>
-      
+    <section className='max-w-7x1 mx-auto'>
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px] ">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]" >Create imaginative and visually stunning images through  Open  AI By Ajay  and showcase them through community </p>
+        <form className='mt-16 max-w-3xl' onSubmit={handleSubmit} >
+          <div className='flex flex-col gap-5' >
+            <FormField labelName="Yor Name" type="text" name="name" placeholder="Ajay" value={form.name} handleChange={handleChange} />
+            <FormField labelName="prompt" type="text" name="prompt" placeholder="A plush toy robot sitting against a yellow wall" value={form.prompt} handleChange={handleChange} isSurpriseMe handleSurpriseMe={handleSurpriseMe} />
+
+          </div>
+
+        </form>
+      </div>
+
     </section>
   )
 }

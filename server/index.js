b9bfbc2aff1b7156b914573c00bd44ed131697b7
Ajay v <vajay4834@gmail.com>
2023-07-02 12:06:14 +0530

    Mongo db connection added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,28 @@
 import express from 'express';
 import *  as dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './mobgo db/connect.js';
 
 dotenv.config();
     
+const app=express();
+app.use(cors());
+app.use(express.json({limit:'50mb'}));
+
+app.get('/',async(reg,res)=>{
+    res.send('Hello from Ajay');
+});
+
+const startserver = async () => {
+    try{
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080,()=>console.log('server has started on port http://localhost:8080 '))
+
+    } catch (error){
+        console.log(error);
+
+    }
+    
+}
+
+startserver();
